b65b736b432baf28b4fadbbdb2d4b5c416091b74
Maicon Gomes <maicondejesus12@hotmail.com>
2022-11-25 02:50:06 -0300

    Adicionando Angular Material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { RoutingModule } from './routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -18,7 +19,8 @@ import { RoutingModule } from './routing.module';
     BrowserModule,
     AppRoutingModule,
     RoutingModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
